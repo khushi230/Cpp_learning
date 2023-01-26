864dc745dbf6ed79d7bf4a95610aaa358acfbe71
khushi230 <khushboosirsa99@gmail.com>
2023-01-26 21:15:50 +0530

    arrange numbers so that all odd come first and even at last

---
+++ array/arrangeOddEven.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+void arrange(int *arr, int n)
+{
+    int temp = 0;
+    int temp2 = 1;
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = 2 * i + 1;
+
+        if (arr[i] > n)
+        {
+            if (n % 2 == 0)
+            {
+                arr[i] = n - temp;
+                temp += 2;
+            }
+            else
+            {
+                arr[i] = n - temp2;
+                temp2 += 2;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int *arr = new int[n];
+        arrange(arr, n);
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        delete[] arr;
+    }
+}
