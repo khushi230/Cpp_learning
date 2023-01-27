9f33cbad3cacb20efafc0f70d422a157dc0e14e9
khushi230 <khushboosirsa99@gmail.com>
2023-01-27 11:39:42 +0530

    find unique number in array

---
+++ array/findUnique.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int findUnique(int *arr, int size)
+{
+    int temp;
+    for (int i = 0; i < size; i++)
+    {
+        int count = 0;
+        for (int j = 0; j < size; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                count++;
+            }
+        }
+        if (count == 1)
+        {
+            temp = arr[i];
+        }
+    }
+    return temp;
+}
+
+int main()
+{
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int size;
+        cin >> size;
+        int *input = new int[size];
+
+        for (int i = 0; i < size; ++i)
+        {
+            cin >> input[i];
+        }
+
+        cout << findUnique(input, size) << endl;
+    }
+
+    return 0;
+}
